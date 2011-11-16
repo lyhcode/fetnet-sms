db9d61874a065362d9c2d9b65ddd67fcbb68bd0b
lyhcode (Yan-hong Lin) <lyhcode@gmail.com>
2011-11-16 16:21:18 +0800

    updated

--- mock-webserver/build/tmp/jettyRunWar/webapp/smssubmit.groovy
+++
@@ -1,9 +0,0 @@
-response.contentType = 'text/xml; charset=UTF-8'
-
-println '''<?xml version="1.0" encoding="UTF-8"?>
-<SubmitRes>
-<MessageId>A13D98F</MessageId>
-<ResultCode>00000</ResultCode>
-<ResultText>Message accepted for processing.</ResultText>
-</SubmitRes>
-'''
